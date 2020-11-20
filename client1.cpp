a937b1dbd81062c1bea2101ec63164ae89a39074
345467 <74457544+345467@users.noreply.github.com>
2020-11-20 23:33:53 +0800

    窗口更新-1.1
    修复所有编译错误
    出现运行时错误：程序返回0xC0000005
    有待修复

--- client1.cpp
+++ client1.cpp
@@ -22,41 +22,46 @@ static TextFieldArea recvs;
 
 class MP:public MessageProcesser{
 	public:
-		Cfg common_cfg;
-		virtual void OnInit(){
-			ip.setText(common_cfg.get("lang.zh_cn.textfield.ip"));
-			msg.setText(common_cfg.get("lang.zh_cn.textarea.msg"));
-			recvs.setText(common_cfg.get("lang.zh_cn.textarea.recvs"));
+		Cfg *common_cfg;
+		MP():MessageProcesser(){
 		}
-		virtual void OnCommand(){}
+		virtual void OnInit(){
+			ip.setText(common_cfg->get("lang.zh_cn.textfield.ip"));
+			msg.setText(common_cfg->get("lang.zh_cn.textarea.msg"));
+			recvs.setText(common_cfg->get("lang.zh_cn.textarea.recvs"));
+		}
+		virtual void OnCommand(){return;}
 		virtual void OnDestory(){
 			running=false;
 		}
-		virtual void OnPaint(){}
-}
-static const MP *mp=new MP;
-static Window<mp> *win;
+		virtual void OnPaint(){return;}
+		virtual string to_string(){
+			return "MP("+std::to_string(int(this))+")";
+		}
+};
+static MP mp;
+static Window *win;
 static Label ipl("");
 static Label msgl("");
 static Button bsend("",onSend);
 static Button brestart("",[]()->void {
     running=false;
     Socket so("127.0.0.1",31126);
-    so.send("退出");
+    so.send("閫€鍑?);
     so.close();
     delete win;
-    win=new Window();
+    win=new Window("TalkTalk0.0.1 - for C++",&mp);
     void cleanup();
     cleanup();
     do_restartable_init();
 });
 
 static void onSend() {
-    logger.info("连接地址:"+ip.getText());
-    logger.info("发送消息:"+msg.getText());
+    logger.info("杩炴帴鍦板潃:"+ip.getText());
+    logger.info("鍙戦€佹秷鎭?"+msg.getText());
     Socket out(ip.getText().c_str(),31126);
     if(*(out.getState())==error) {
-        logger.error("发送失败!");
+        logger.error("鍙戦€佸け璐?");
         return;
     }
     out.send(msg.getText());
@@ -66,37 +71,37 @@ static void onSend() {
 static int listener_thread(LPVOID lpParamter) {
     ServerSocket server(31126);
     Logger inner("[LISTENER_THREAD]");
-    inner.info("开始监听...");
+    inner.info("寮€濮嬬洃鍚?..");
     while(running) {
         Socket so=server.accept();
-        inner.info("得到连接");
+        inner.info("寰楀埌杩炴帴");
         string str=so.recv();
-        inner.info("数据:"+str);
+        inner.info("鏁版嵁:"+str);
         recvs.setText(recvs.getText()+"\n"+to_string(
                           so.getAddr()->sin_port)
                       +":"+str);
-        SendMessage(hwnd,UWM_NULL,0,0);
+        win->update();
     }
-    inner.info("退出!\n");
+    inner.info("閫€鍑?\n");
     server.close();
     return 0;
 }
 
 void do_only_once_init(){
     //cout<<HttpRequest("http://changxiang.nat123.net",80,"/talktalk.html?from=a&to=b&msg=some%20text");
-    logger.info("初始化监听线程");
+    logger.info("鍒濆鍖栫洃鍚嚎绋?);
     Thread(listener_thread).start();
 }
 
 void do_restartable_init(){
 	Log::init("log.log");
     Log::setLevel(INFO);
-    logger.info("初始化网络");
+    logger.info("鍒濆鍖栫綉缁?);
     use_net();
-	logger.info("读取设置文件:common.cfg");
+	logger.info("璇诲彇璁剧疆鏂囦欢:common.cfg");
     Cfg common_cfg("common.cfg",default_common_cfg);
     
-    logger.info("设置UI部件尺寸");
+    logger.info("璁剧疆UI閮ㄤ欢灏哄");
     Log::setLevel(levelFromString(common_cfg.get("log.level")));
     ipl.setBounds(sti(common_cfg.get("gui.label.ip.x")),
 				 sti(common_cfg.get("gui.label.ip.y")));
@@ -125,15 +130,15 @@ void do_restartable_init(){
 	
 	logger.info(bsend.to_string());
 	
-	logger.info("加载语言");
+	logger.info("鍔犺浇璇█");
 	ipl.setText(common_cfg.get("lang.zh_cn.label.ip"));
 	msgl.setText(common_cfg.get("lang.zh_cn.label.msg"));
 	bsend.setText(common_cfg.get("lang.zh_cn.button.send"));
 	brestart.setText(common_cfg.get("lang.zh_cn.button.restart"));
 	
-	logger.info("注册UI部件");
-	mp->common_cfg=common_cfg;
-	win=new Window("TalkTalk0.0.1 - for C++");
+	logger.info("娉ㄥ唽UI閮ㄤ欢");
+	mp.common_cfg=&common_cfg;
+	win=new Window("TalkTalk0.0.1 - for C++",&mp);
     win->addLabel(&ipl);
     win->addLabel(&msgl);
     win->addButton(&bsend);
@@ -145,7 +150,7 @@ void do_restartable_init(){
 
 void cleanup(){
 	unuse_net();
-    logger.info("退出!\n");
+    logger.info("閫€鍑?\n");
     Log::exit();
 }
 
@@ -157,13 +162,14 @@ void appMain(){
 }
 //server.natappfree.cc
 int main(int argc, char* argv[]) {
+	cout<<"1"<<endl; 
 	try{
 		appMain();
 	}catch(exception &e){
-		logger.fatal("发现未捕捉的异常!");
-		logger.fatal(string("详细信息:")+e.what());
+		logger.fatal("鍙戠幇鏈崟鎹夌殑寮傚父!");
+		logger.fatal(string("璇︾粏淇℃伅:")+e.what());
 	}catch(...){
-		logger.fatal("发现未知异常!");
+		logger.fatal("鍙戠幇鏈煡寮傚父!");
 	}
     return 0;
 }
